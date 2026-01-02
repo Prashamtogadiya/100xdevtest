d7e77071631e6b92b9e630bbae92389c50550d07
Prashamtogadiya <pktogadiya222@gmail.com>
2026-01-02 23:24:41 +0530

    added sign up route and validation

---
+++ http/src/index.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import { SignupSchema } from "./types.js";
+import { UserModel } from "./models.js";
+
+const app = express()
+
+app.use(express.json())
+
+
+app.post('/auth/signup', async (req, res) => {
+    const { success, data } = SignupSchema.safeParse(req.body);
+    if (!success) {
+        return res.status(400).json({
+            "success": false,
+            "error": "Invalid request schema",
+        })
+    }
+
+    const user = await UserModel.findOne({ email: data.email })
+    if (user) {
+        return res.status(400).json({
+            "success": false,
+            "error": "Email already exists"
+        })
+    }
+
+    const userDb = await UserModel.create({
+        email:data.email,
+        password:data.password,
+        name:data.name
+    })
+    res.json( {
+        success:true,
+        data:{
+            _id:userDb._id,
+            name:userDb.name,
+            email:userDb.email,
+            password:userDb.password
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log("server started on port 3000");
+
+})
