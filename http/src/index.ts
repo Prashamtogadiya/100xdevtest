036bd022c39c882a4ac6d0bab70670c9eec3f510
Prashamtogadiya <pktogadiya222@gmail.com>
2026-01-08 19:07:53 +0530

    added auth middleware and extended Express Request interface with userId and role

--- http/src/index.ts
+++ http/src/index.ts
@@ -3,77 +3,89 @@ import { SignupSchema, SingInSchema } from "./types.js";
 import { UserModel } from "./models.js";
 import mongoose, { mongo } from "mongoose";
 import jwt from "jsonwebtoken";
-const app = express()
+import { authMiddleware } from "./middleware.js";
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
+app.post("/auth/signup", async (req, res) => {
+  const { success, data } = SignupSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(400).json({
+      success: false,
+      error: "Invalid request schema",
+    });
+  }
 
-app.post('/auth/signup', async (req, res) => {
-    const { success, data } = SignupSchema.safeParse(req.body);
-    if (!success) {
-        return res.status(400).json({
-            "success": false,
-            "error": "Invalid request schema",
-        })
-    }
+  const user = await UserModel.findOne({ email: data.email });
+  if (user) {
+    return res.status(400).json({
+      success: false,
+      error: "Email already exists",
+    });
+  }
 
-    const user = await UserModel.findOne({ email: data.email })
-    if (user) {
-        return res.status(400).json({
-            "success": false,
-            "error": "Email already exists"
-        })
-    }
+  const userDb = await UserModel.create({
+    email: data.email,
+    password: data.password,
+    name: data.name,
+  });
+  res.json({
+    success: true,
+    data: {
+      _id: userDb._id,
+      name: userDb.name,
+      email: userDb.email,
+      password: userDb.password,
+    },
+  });
+});
 
-    const userDb = await UserModel.create({
-        email: data.email,
-        password: data.password,
-        name: data.name
-    })
-    res.json({
-        success: true,
-        data: {
-            _id: userDb._id,
-            name: userDb.name,
-            email: userDb.email,
-            password: userDb.password
-        }
-    })
-})
+app.post("/auth/login", async (req, res) => {
+  const { success, data } = SingInSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(400).json({
+      success: false,
+      error: "Invalid request schema",
+    });
+  }
 
-app.post('/auth/login', async (req, res) => {
-    const { success, data } = SingInSchema.safeParse(req.body);
-    if (!success) {
-        return res.status(400).json({
-            "success": false,
-            "error": "Invalid request schema",
-        })
-    }
+  const userDb = await UserModel.findOne({
+    email: data.email,
+  });
+  if (!userDb || userDb.password != data.password) {
+    return res.status(400).json({
+      success: false,
+      error: "Invalid Email or Password",
+    });
+  }
 
-    const userDb = await UserModel.findOne({
-        email: data.email
-    })
-    if (!userDb || userDb.password != data.password) {
-        return res.status(400).json({
-            "success": false,
-            "error": "Invalid Email or Password"
-        })
-    }
+  const token = await jwt.sign(
+    {
+      role: userDb.role,
+      userId: userDb._id,
+    },
+    process.env.JWT_SECRETKEY!
+  );
 
-    const token = await jwt.sign({
-        role: userDb.role,
-        userId: userDb._id
-    }, process.env.JWT_SECRETKEY!)
+  res.json({
+    success: true,
+    data: {
+      token,
+    },
+  });
+});
 
-    res.json({
-        success: true,
-        data: {
-            token
-        }
-    })
-})
+app.post("/auth/me", authMiddleware, async (req, res) => {
+  const user = await UserModel.findOne({ _id: req.userId });
+    if (!user) {
+    return res.status(404).json({
+      success: false,
+      error: "User not found",
+    });
+  }
+});
 
 app.listen(3000, () => {
-    console.log("server started on port 3000");
-
-})
+  console.log("server started on port 3000");
+});
---
+++ http/src/middleware.ts
@@ -0,0 +1,24 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+export const authMiddleware = async(req:Request,res:Response, next:NextFunction)=>{
+    const token = req.headers.authorization ;
+    if(!token){
+        return res.status(401).json({
+            "success":false,
+            "error":"Unauthorized, token missing or invalid"
+        })
+    }
+    console.log(typeof token );
+    
+    try{
+        const {userId,role}= await jwt.verify(token,process.env.JWT_SECRETKEY!) as JwtPayload 
+        req.userId = userId ;
+        req.role = role ;
+        next()
+    }catch(err){
+       return res.status(401).json({
+            "success":false,
+            "error":"Unauthorized, token missing or invalid"
+        })
+    }
+}
